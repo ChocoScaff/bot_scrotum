8bd70bb18fde0951e194c947b2550c1592c9d784
ChocoScaff <60853783+ChocoScaff@users.noreply.github.com>
2021-01-27 19:57:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max)); // Fonction math pour aléatoire
   }
 
-var mots = ["Le racisme c'est la peur des étrangers, donc si y'avais moins d'étrangers y'aurai moins racisme.", "L'amour ça fait mal au coeur et au cul.", "On à pas tous les outils qui nous faut pour reussir, y'a ceux qui ont un rateau pour enlever les feuilles mort moi j'ai une pelle.","On m’a dit en gros l’asile c’est en dehors de la ville et déjà l’asile c’est un isolement en soit, bah le mettre loin comme ça c’est pas anodin, tu vois. être fou c’est être isolé, incompris, et on vit dans une societer.","On attend souvent l'épisode 3 d'une série avec impatience mais parfois faut mieux pas avoir de suite plutôt qu'être déçu."];
+var mots = ["Le racisme c'est la peur des étrangers, donc si y'avais moins d'étrangers y'aurai moins racisme.", "L'amour ça fait mal au coeur et au cul.", "On a pas tous les outils qui nous faut pour reussir, y'a ceux qui ont un rateau pour enlever les feuilles mort moi j'ai une pelle.","On m’a dit en gros l’asile c’est en dehors de la ville et déjà l’asile c’est un isolement en soit, bah le mettre loin comme ça c’est pas anodin, tu vois. être fou c’est être isolé, incompris, et on vit dans une societer.","On attend souvent l'épisode 3 d'une série avec impatience mais parfois faut mieux pas avoir de suite plutôt qu'être déçu."];
 var aléatoire;
 
 client.on('ready', () => {
