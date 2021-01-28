e2fd529d89ceb1b2ba0c32d80d66e661d6a5a341
ChocoScaff <60853783+ChocoScaff@users.noreply.github.com>
2021-01-28 11:55:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,12 @@ var mots =
     "Un poulpe m'a montré son scrotum dans ça camionnette ",
     "Mon père m'a dit les noirs c'est tous des voleurs, mais je ne comprend pas les personnes noir n'existe pas ?!"
     "hyperflex m'a dit que si a trente ans t'as pas de loupitaf t'as raté t'as vie"
+    "De nos jours, trop de je t'aime sont prononcés comme les bonjours ",
+    "Cette chanson n'a aucun sens et c'est ce qui fait kiffer",
+    "J'ai jamais présenté quelqu'un à mes parentset heuresement",
+    "L'amour platonique est inacceptable dans la mesure ou il est irééel, et qu'à mon sens, seul l'amour réciproque est vrai.",
+    "Je me pose la vraiment la question, je sais pas si c'est la haine ou si vraiment j'ai beaucoup moins de sentiments",
+    "Tout le temps je dis je t'aime aux gens maintenant. c'est trop important de le dire,je m'en rend à présent."
 ];
 
 client.on('ready', () => {
