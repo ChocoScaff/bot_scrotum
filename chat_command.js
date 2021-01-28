a1066e087cf6b31ef35a2d9637eb82aecc36ff31
YourLemon <35894149+YourLemon@users.noreply.github.com>
2021-01-28 22:16:46 +0100

    Update chat_command.js

--- chat_command.js
+++ chat_command.js
@@ -16,7 +16,7 @@ function __s_ChatCmd_stepipoule(pChannel, args)
 {
 	pChannel.send(RandomInt(0, szMessageArray.lenght));
 }
-const CChatCommand(stepipoule, __s_ChatCmd_stepipoule);
+const CChatCommand("stepipoule", __s_ChatCmd_stepipoule);
 
 var iLastTime = -1;
 
@@ -79,4 +79,4 @@ function __s_ChatCmd_legay(pChannel, args)
 	pChannel.send(szMsg);
 	iLastTime = Date.now();
 }
-const CChatCommand(legay, __s_ChatCmd_legay);
+const CChatCommand("legay", __s_ChatCmd_legay);
