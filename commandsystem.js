b4059a369c89e6496a6df420ba84179584b30187
YourLemon <35894149+YourLemon@users.noreply.github.com>
2021-01-29 19:04:18 +0100

    Update commandsystem.js

--- commandsystem.js
+++ commandsystem.js
@@ -1,39 +1,61 @@
+class CChatCommand
+{	
+	constructor(pszCmdName, pfnFunc)
+	{
+		this.m_pszCmdName = pszCmdName;
+		this.m_pfnFunc = pfnFunc;
+	}
+}
 
-
-var m_iRegisterCount = 0;
-var m_CCmdRegister = [];
 class CCommandSystem
 {
-	constructor() { }
+	constructor()
+	{
+		this.m_iRegisterCount = 0;
+		this.m_CCmdRegister = [];
+	}
 	
 	RegisterCCmd(pszCmdName, pfnFunc)
 	{
-		console.log("CCommandSystem::RegisterCCmd " + pfnFunc + "_" + pszCmdName);
+		console.log("CCommandSystem::RegisterCCmd " + pszCmdName + " at " + this.m_iRegisterCount);
 		
-		m_CCmdRegister[m_iRegisterCount] == {};
-		m_CCmdRegister[m_iRegisterCount] == { pszCmdName, pfnFunc };
-		m_iRegisterCount++;
+		var ChatCmd = new CChatCommand(pszCmdName, pfnFunc);
+		
+		this.m_CCmdRegister[this.m_iRegisterCount] = ChatCmd;
+		this.m_iRegisterCount++;
 	}
 	
 	OnReceiveCommand(pChannel, pszCmd, pszArgs)
 	{
-		console.log("CCommandSystem::OnReceiveCommand " + pChannel + " " + pszCmd + " " + pszArgs);
-		
-		for (var i = 0; i < m_iRegisterCount; i++)
+		for (var i = 0; i < this.m_iRegisterCount; i++)
 		{
-			if (m_CCmdRegister[i].pszCmdName === pszCmd)
+			var ChatCmd = this.m_CCmdRegister[i];
+			if (ChatCmd.m_pszCmdName === pszCmd)
 			{
-				m_CCmdRegister[i].pfnFunc(pChannel, pszArgs);
+				ChatCmd.m_pfnFunc(pChannel, pszArgs);
+				console.log("CCommandSystem::OnReceiveCommand command received : " + pszCmd);
 				return;
 			}
 		}
 		
 		console.log("CCommandSystem::OnReceiveCommand unknown command : " + pszCmd);
 	}
+	
+	PrintCommand()
+	{
+		var szMsg = "Moi je sais répondre a ça ; \n"
+		var szInterMsg = "";
+		for (var i = 0; i < this.m_iRegisterCount; i++)
+		{
+			var ChatCmd = this.m_CCmdRegister[i];
+			szMsg += ChatCmd.m_pszCmdName + "\n";
+			szInterMsg += i + " " + ChatCmd.m_pszCmdName + " ";
+		}
+		
+		console.log("Command List : " + szInterMsg);
+		return szMsg;
+	}
 }
 
-const sCmd = new CCommandSystem();
-function CmdSys()
-{
-	return sCmd;
-}
+const gCmdSys = new CCommandSystem;
+module.exports.gCommandSystem = gCmdSys;
