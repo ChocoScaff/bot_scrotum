17613cac4a490fa50985abbaf3c759dc165f4965
YourLemon <35894149+YourLemon@users.noreply.github.com>
2021-01-29 19:04:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,40 @@
+console.log("bot::Scope entry");
+
 const hDiscord = require('discord.js');
 const hConfig = require("./config.json");
-const hCmdSys = require('./commandsystem.js');
-require('./chat_command.js');
+const hCmdSys = require("./commandsystem.js");
+require("./chat_command.js");
+
+console.log("bot::Scope include");
 
 const gClient = new hDiscord.Client();
+var gCmdSys = hCmdSys.gCommandSystem;
 
-gClient.on("message", function(message)
-{
-	console.log("hook::OnMessage " + message);
-	
-	if (message.author.bot)
-		return;
-	
-	if (!message.content.startsWith(hConfig.CMD_PREFIX))
-		return;
+console.log("bot::Scope declare");
+
+gClient.on("message",
+	function(message)
+	{
+		console.log("Client::OnMessage " + message);
 		
-	var pszCmdLine = message.content.slice(hConfig.CMD_PREFIX.length);
-	var pszArgs = pszCmdLine.split(' ');
-	var pszCommand = args.shift().toLowerCase();
-	
-	hCmdSys.CmdSys().OnReceiveCommand(message.channel ,pszCommand, pszArgs);
-});
+		if (message.author.bot)
+			return;
+		
+		if (!message.content.startsWith(hConfig.CMD_PREFIX))
+			return;
+			
+		var pszCmdLine = message.content.slice(hConfig.CMD_PREFIX.length);
+		var pszArgs = pszCmdLine.split(' ');
+		var pszCommand = args.shift().toLowerCase();
+		
+		gCmdSys.OnReceiveCommand(message.channel, pszCommand, pszArgs);
+	}
+);
+
+console.log("bot::Scope hook");
 
 gClient.login('Token_discord');
+
+console.log("bot::Scope startup");
+
+gCmdSys.PrintCommand();
