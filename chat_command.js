aa3f296e5bea733c7f167cdeb565772af5a5432b
ChocoScaff <60853783+ChocoScaff@users.noreply.github.com>
2021-02-10 18:10:46 +0100

    Update chat_command.js

--- chat_command.js
+++ chat_command.js
@@ -76,7 +76,8 @@ const szMessageArraystepipoule =
     "Hier j'ai appelé mon wifi ''hack me if you can''. Aujourd'hui il sappelle ''challenge accepted''. ",
     "Pour vous je suis une Poule ou un Poulet ? Attention à pas confondre genre et identité de genre !!",
     "La vie c'est comme un penis, elle est courte et les femmes la rendent dure sans raison",
-    "les hommes hétérosexuels savent très bien que la misogynie ça dégoûte les femmes mais ils continuent à faire des blagues misogynes pour avoir la validations des autres hommes ? c’est juste que vous êtes homosexuels je pense" 
+    "les hommes hétérosexuels savent très bien que la misogynie ça dégoûte les femmes mais ils continuent à faire des blagues misogynes pour avoir la validations des autres hommes ? c’est juste que vous êtes homosexuels je pense",
+    "Mon opinion sur l'avortement est comme une lame à double tranchant. Je veux dire, j'aime l'idée de tuer des bébés, mais je n'aime pas l'idée de donner aux femmes la chance de prendre des décisions "
 ];
 
 function __s_ChatCmd_stepipoule(pChannel, args)
